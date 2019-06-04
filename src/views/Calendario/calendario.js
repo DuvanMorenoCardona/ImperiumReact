f4bac9ec6e14e3876b369f40e4e483ec4150d177
DuvanMoreno <duvanmorenocardona.dmc@gmail.com>
2019-06-03 22:36:56 -0500

    Agrega boton +Event y trae info de Firebase

--- src/views/Calendario/calendario.js
+++ src/views/Calendario/calendario.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import './calendario.css';
+import "./calendario.css";
 
 // Importar componentes
 import NavBar from "../../components/NavBar/NavBar";
@@ -9,6 +9,14 @@ import Fullcalendar from "../../components/Fullcalendar/fullcalendar";
 
 // Importar componentes de Material-ui
 import { withStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Icon from "@material-ui/core/Icon";
+import DeleteIcon from "@material-ui/icons/Delete";
+import NavigationIcon from "@material-ui/icons/Navigation";
+
+// Importar componentes externos
+import firebase from "firebase";
 
 const styles = theme => ({
   root: {
@@ -42,13 +50,39 @@ export default withStyles(styles)(
   class Calendario extends Component {
     constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {
+        Events: {}
+      };
     }
+
+    componentWillMount() {
+      const EventsFirebase = firebase
+        .database()
+        .ref()
+        .child("Evento");
+      var me = this;
+      EventsFirebase.on("value", snapshot => {
+        // console.log(snapshot.val());
+        me.setState({
+          Events: snapshot.val()
+        });
+      });
+      console.log(this.state.Events);
+    }
+
     render() {
       const { classes } = this.props;
       return (
         <div className="content-calendar" style={{ height: "100%" }}>
           <Fullcalendar />
+          <Fab
+            variant="extended"
+            className="btn-addevent"
+            aria-label="add "
+          >
+            <AddIcon />
+            Evento
+          </Fab>
         </div>
       );
     }
