46529cedccc4a322f60358fce244b295d6eb72c4
DuvanMoreno <duvanmorenocardona.dmc@gmail.com>
2019-06-05 11:02:33 -0500

    Agrega Textfiles incompleta en el modal

--- src/molecules/ButtonFloat/ButtonFloat.js
+++ src/molecules/ButtonFloat/ButtonFloat.js
@@ -12,33 +12,39 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import withMobileDialog from "@material-ui/core/withMobileDialog";
 import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import NavigationIcon from "@material-ui/icons/Add";
 
 export default class ButtonFloat extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        open: false
-    }
+      open: true
+    };
 
-      this.handleClickOpen = this.handleClickOpen.bind(this);
-
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
-    handleClickOpen() {
-        this.setState({
-            open: true
-        });
-    }
+  handleClickOpen() {
+    this.setState({
+      open: true
+    });
+  }
 
-    handleClose() {
-        this.setState({
-            open: false
-        });
-    }
+  handleClose() {
+    this.setState({
+      open: false
+    });
+  }
 
   render() {
-
     return (
       <div>
         <Fab
@@ -55,10 +61,102 @@ export default class ButtonFloat extends Component {
           aria-labelledby="form-dialog-title"
           fullWidth
         >
-          <DialogTitle id="form-dialog-title">
-            Editar Contacto Monitor
-          </DialogTitle>
-          <DialogContent />
+          <DialogTitle id="form-dialog-title">Agregar Evento</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              name="name"
+              label="Nombre de Evento"
+              type="text"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              name="Dependence"
+              label="Dependecia"
+              type="text"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              name="Charge"
+              label="Encargado del Evento"
+              type="text"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              name="Cost"
+              label="Centro de costos"
+              type="text"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              name="Rubro"
+              label="Rubro"
+              type="text"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              name="Extent"
+              label="Extensión"
+              type="text"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              name="Monitors"
+              label="Cantidad de monitores"
+              type="number"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              name="Extent"
+              label="Extensión"
+              type="text"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              name="Date1"
+              label="Fecha inicio"
+              type="date"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              name="Date2"
+              label="Fecha fin"
+              type="date"
+            />
+            <Typography>Horario</Typography>
+
+            <FormControl>
+              <InputLabel htmlFor="age-simple">Day</InputLabel>
+              <Select
+                value={this.state.date}
+                onChange={this.handleChange}
+                inputProps={{
+                  name: "age",
+                  id: "age-simple"
+                }}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={"Lunes"}>Lunes</MenuItem>
+                <MenuItem value={"Martes"}>Martes</MenuItem>
+                <MenuItem value={"Miercoles"}>Miércoles</MenuItem>
+                <MenuItem value={"Jueves"}>Jueves</MenuItem>
+                <MenuItem value={"Viernes"}>Viernes</MenuItem>
+                <MenuItem value={"Sabado"}>Sabado</MenuItem>
+              </Select>
+            </FormControl>
+          </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClose} color="primary">
               Cancelar
