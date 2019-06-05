c0139ed3703098623445add7ecdf9a74702b59ba
DuvanMoreno <duvanmorenocardona.dmc@gmail.com>
2019-06-05 16:58:59 -0500

    Agrega eventos a Firebase

--- src/molecules/ButtonFloat/ButtonFloat.js
+++ src/molecules/ButtonFloat/ButtonFloat.js
@@ -20,16 +20,38 @@ import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import NavigationIcon from "@material-ui/icons/Add";
 
+// importar componentes externos
+import firebase from "firebase";
+
 export default class ButtonFloat extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      open: true
+      // Variable para abrir cerrar Model
+      open: true,
+      // Variables de model
+      nameEvent: "",
+      dependence: "",
+      change: "",
+      nameCharge: "",
+      extent: "",
+      cost: "",
+      rubro: "",
+      date1: "",
+      date2: "",
+      observation: "",
+      monitors: "",
+      hour1: "",
+      hour2: ""
     };
-
+    
+    // Funciones de model
     this.handleClickOpen = this.handleClickOpen.bind(this);
     this.handleClose = this.handleClose.bind(this);
+    this.handleSendDatabase = this.handleSendDatabase.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+
   }
 
   handleClickOpen() {
@@ -41,6 +63,42 @@ export default class ButtonFloat extends Component {
   handleClose() {
     this.setState({
       open: false
+    });
+  }
+
+  handleSendDatabase() {
+    firebase
+      .database()
+      .ref("Evento/" + this.state.nameEvent)
+      .set({
+        nombreEvento: this.state.nameEvent,
+        dependecia: this.state.dependence,
+        encargado: {
+          nombre: this.state.nameCharge,
+          extension: this.state.extent
+        },
+        centroDeCostos: this.state.cost,
+        rubro: this.state.rubro,
+        fechaInicioEvento: this.state.date1,
+        fechaFinEvento: this.state.date2,
+        horaInicio: this.state.hour1,
+        horaFin: this.state.hour2,
+        observaciones: this.state.observation
+      });
+    console.log("Enviado");
+
+    this.setState({
+      open: false
+    });
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+
+    this.setState({
+      [name]: value
     });
   }
 
@@ -66,96 +124,103 @@ export default class ButtonFloat extends Component {
             <TextField
               autoFocus
               margin="dense"
-              name="name"
+              name="nameEvent"
+              value={this.state.nameEvent}
               label="Nombre de Evento"
               type="text"
+              onChange={this.handleInputChange}
             />
             <TextField
               autoFocus
               margin="dense"
-              name="Dependence"
+              name="dependence"
               label="Dependecia"
+              value={this.state.dependence}
+              onChange={this.handleInputChange}
               type="text"
             />
             <TextField
               autoFocus
               margin="dense"
-              name="Charge"
+              name="nameCharge"
+              value={this.state.nameCharge}
+              onChange={this.handleInputChange}
               label="Encargado del Evento"
               type="text"
             />
             <TextField
               autoFocus
               margin="dense"
-              name="Cost"
+              name="cost"
+              value={this.state.cost}
+              onChange={this.handleInputChange}
               label="Centro de costos"
               type="text"
             />
             <TextField
               autoFocus
               margin="dense"
-              name="Rubro"
+              name="rubro"
+              value={this.state.rubro}
+              onChange={this.handleInputChange}
               label="Rubro"
               type="text"
             />
             <TextField
               autoFocus
               margin="dense"
-              name="Extent"
+              name="extent"
+              value={this.state.extent}
+              onChange={this.handleInputChange}
               label="Extensión"
               type="text"
             />
             <TextField
               autoFocus
               margin="dense"
-              name="Monitors"
+              name="monitors"
+              value={this.state.monitors}
+              onChange={this.handleInputChange}
               label="Cantidad de monitores"
               type="number"
             />
             <TextField
               autoFocus
               margin="dense"
-              name="Extent"
-              label="Extensión"
-              type="text"
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              name="Date1"
+              name="date1"
+              value={this.state.date1}
+              onChange={this.handleInputChange}
               label="Fecha inicio"
               type="date"
             />
             <TextField
               autoFocus
               margin="dense"
-              name="Date2"
+              name="date2"
+              value={this.state.date2}
+              onChange={this.handleInputChange}
               label="Fecha fin"
               type="date"
             />
             <Typography>Horario</Typography>
-
-            <FormControl>
-              <InputLabel htmlFor="age-simple">Day</InputLabel>
-              <Select
-                value={this.state.date}
-                onChange={this.handleChange}
-                inputProps={{
-                  name: "age",
-                  id: "age-simple"
-                }}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={"Lunes"}>Lunes</MenuItem>
-                <MenuItem value={"Martes"}>Martes</MenuItem>
-                <MenuItem value={"Miercoles"}>Miércoles</MenuItem>
-                <MenuItem value={"Jueves"}>Jueves</MenuItem>
-                <MenuItem value={"Viernes"}>Viernes</MenuItem>
-                <MenuItem value={"Sabado"}>Sabado</MenuItem>
-              </Select>
-            </FormControl>
+            <TextField
+              autoFocus
+              margin="dense"
+              name="hour1"
+              value={this.state.hour1}
+              onChange={this.handleInputChange}
+              label="hora de inicio"
+              type="time"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              name="hour2"
+              value={this.state.hour2}
+              onChange={this.handleInputChange}
+              label="hora de fin"
+              type="time"
+            />
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClose} color="primary">
